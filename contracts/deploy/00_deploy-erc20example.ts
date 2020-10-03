ac153bfdfdf732de374d71394fbc269cfa78e4d3
dhadrien <hadrien@variabl.io>
2020-10-03 13:32:10 +0200

    Linting

--- contracts/deploy/00_deploy-erc20example.ts
+++ contracts/deploy/00_deploy-erc20example.ts
@@ -1,7 +1,10 @@
-import {BuidlerRuntimeEnvironment, DeployFunction} from '@nomiclabs/buidler/types';
-import {ethers} from '@nomiclabs/buidler';
-import {ethers as ethers2} from 'ethers';
-import path from 'path';
+import {
+  BuidlerRuntimeEnvironment,
+  DeployFunction,
+} from "@nomiclabs/buidler/types";
+import {ethers} from "@nomiclabs/buidler";
+import {ethers as ethers2} from "ethers";
+import path from "path";
 
 const {
   utils: {parseEther},
@@ -11,8 +14,13 @@ const func: DeployFunction = async function (bre: BuidlerRuntimeEnvironment) {
   const {deployer, user} = await bre.getNamedAccounts();
   const {deploy} = bre.deployments;
   const useProxy = !bre.network.live;
-  
-  await deploy('ERC20Log', {from: deployer, proxy: false, args: [parseEther('30000'), 'Example Token', 'ETK'], log: true});
+
+  await deploy("ERC20Log", {
+    from: deployer,
+    proxy: false,
+    args: [parseEther("30000"), "Example Token", "ETK"],
+    log: true,
+  });
   return !useProxy; // when live network, record the script as executed to prevent rexecution
 };
 export default func;
--- contracts/deploy/01_init.ts
+++ contracts/deploy/01_init.ts
@@ -1,50 +1,78 @@
-import {BuidlerRuntimeEnvironment, DeployFunction} from '@nomiclabs/buidler/types';
-import {ethers} from '@nomiclabs/buidler';
-import {ethers as ethers2} from 'ethers';
-import {getEnv, DEFAULT_ENV_ADDRESS} from '../utils/envutils';
-import {weiAmountToString} from '../utils/ethutils';
-import path from 'path';
+import {
+  BuidlerRuntimeEnvironment,
+  DeployFunction,
+} from "@nomiclabs/buidler/types";
+import {ethers} from "@nomiclabs/buidler";
+import {ethers as ethers2} from "ethers";
+import {getEnv, DEFAULT_ENV_ADDRESS} from "../utils/envutils";
+import {weiAmountToString} from "../utils/ethutils";
+import path from "path";
 
 const {
   utils: {parseEther},
 } = ethers2;
 
 // 1 Billion Tokens
-const INIT_TOKEN_SUPPLY = parseEther('1000000000');
-const INIT_LIQUIDITY = parseEther('10000');
+const INIT_TOKEN_SUPPLY = parseEther("1000000000");
+const INIT_LIQUIDITY = parseEther("10000");
 const NULL_ADDRESS = "0x0000000000000000000000000000000000000000";
-const INFINITE_DEADLINE= 1000000000000000000
+const INFINITE_DEADLINE = 1000000000000000000;
 
-const env = getEnv(path.join(__dirname, '../.env'));
-const {UNISWAPV2FACTORY_ADDRESS, DAI_ADDRESS, WETH_ADDRESS, UNISWAPV2ROUTERV2_ADDRESS} = env;
+const env = getEnv(path.join(__dirname, "../.env"));
+const {
+  UNISWAPV2FACTORY_ADDRESS,
+  DAI_ADDRESS,
+  WETH_ADDRESS,
+  UNISWAPV2ROUTERV2_ADDRESS,
+} = env;
 const func: DeployFunction = async function (bre: BuidlerRuntimeEnvironment) {
   const {deployer} = await bre.getNamedAccounts();
   const deployerSigner = await ethers.getSigner(deployer);
   const {deploy} = bre.deployments;
   // Uniswap factory, DAI, WETH from mainnet fork
-  const uniFactory = await ethers.getContractAt('UniswapV2Factory', UNISWAPV2FACTORY_ADDRESS || DEFAULT_ENV_ADDRESS, deployerSigner);
-  console.log('------------ DEPLOYING ERC20 TOKEN: STP');
-  await deploy('STP', {contract: 'ERC20Log', from: deployer, proxy: false, args: [INIT_TOKEN_SUPPLY, 'Stop Loss Token', 'STP'], log: true});
-  const STPToken = await ethers.getContract('STP', deployerSigner);
-  console.log('------------ DEPLOYING ERC20 TOKEN: Fake WETH (FWETH)');
-  await deploy('FWETH', {contract: 'ERC20Log', from: deployer, proxy: false, args: [INIT_TOKEN_SUPPLY, 'Stop Loss Token', 'FWETH'], log: true});
-  const FWETH = await ethers.getContract('FWETH', deployerSigner);
-  console.log('------------ CREATING UNISWAP PAIR: FWETH/STP');
+  const uniFactory = await ethers.getContractAt(
+    "UniswapV2Factory",
+    UNISWAPV2FACTORY_ADDRESS || DEFAULT_ENV_ADDRESS,
+    deployerSigner
+  );
+  console.log("------------ DEPLOYING ERC20 TOKEN: STP");
+  await deploy("STP", {
+    contract: "ERC20Log",
+    from: deployer,
+    proxy: false,
+    args: [INIT_TOKEN_SUPPLY, "Stop Loss Token", "STP"],
+    log: true,
+  });
+  const STPToken = await ethers.getContract("STP", deployerSigner);
+  console.log("------------ DEPLOYING ERC20 TOKEN: Fake WETH (FWETH)");
+  await deploy("FWETH", {
+    contract: "ERC20Log",
+    from: deployer,
+    proxy: false,
+    args: [INIT_TOKEN_SUPPLY, "Stop Loss Token", "FWETH"],
+    log: true,
+  });
+  const FWETH = await ethers.getContract("FWETH", deployerSigner);
+  console.log("------------ CREATING UNISWAP PAIR: FWETH/STP");
   let pairAddress = await uniFactory.getPair(STPToken.address, FWETH.address);
-  if(pairAddress === NULL_ADDRESS) {
-    await (await uniFactory.createPair(STPToken.address, FWETH.address)).wait()
+  if (pairAddress === NULL_ADDRESS) {
+    await (await uniFactory.createPair(STPToken.address, FWETH.address)).wait();
     pairAddress = await uniFactory.getPair(STPToken.address, FWETH.address);
-    console.log('Pair Created: ', pairAddress);
-  } else console.log('Pair already created in previous deployment: ', pairAddress);
-  console.log('------------ ADDING INITIAL LIQUIDITY');
-  const pair = await ethers.getContractAt('UniswapV2Pair', pairAddress, deployerSigner);
+    console.log("Pair Created: ", pairAddress);
+  } else
+    console.log("Pair already created in previous deployment: ", pairAddress);
+  console.log("------------ ADDING INITIAL LIQUIDITY");
+  const pair = await ethers.getContractAt(
+    "UniswapV2Pair",
+    pairAddress,
+    deployerSigner
+  );
   const uniRouter = await ethers.getContractAt(
-    'UniswapV2Router02',
+    "UniswapV2Router02",
     UNISWAPV2ROUTERV2_ADDRESS || DEFAULT_ENV_ADDRESS,
     deployerSigner
   );
   // await uniRouter.addLiquidity(FWETH.address, STPToken.address, INIT_LIQUIDITY, INIT_LIQUIDITY, INIT_LIQUIDITY, INIT_LIQUIDITY, deployer, INFINITE_DEADLINE, {dkzalzamk: "haha" })
-
 
   // // INIT
   // console.log(`------------ USER: ${user}`);
@@ -80,12 +108,12 @@ const func: DeployFunction = async function (bre: BuidlerRuntimeEnvironment) {
   // await SLfactory.on('PoolCreated', (token0, token1, pair, pool, uint) =>{
   //   console.log(`
   //   STOP LOSS Pool #${uint} created: ${pool}
-  //   Token 1 ${token0} 
+  //   Token 1 ${token0}
   //   Token 2 ${token1}
   //   Uniswap Pair: ${pair}
   //   `)
   // })
-  
+
   // console.log('------------ CREATING STOP LOSS POOL WETH/DAI');
   // const txCreatePool = await SLfactory.createPool(WETH_ADDRESS, DAI_ADDRESS);
   // await txCreatePool.wait();
@@ -94,7 +122,7 @@ const func: DeployFunction = async function (bre: BuidlerRuntimeEnvironment) {
   // await SLPool.on('StopLoss', (uniPair, orderer, lpamount, token, amount) =>{
   //   console.log(`
   //   StopLoss Ordered UniPair: ${uniPair}
-  //   0rderer: ${orderer} 
+  //   0rderer: ${orderer}
   //   LP Amount sent: ${lpamount}
   //   Token to guarantee: ${token}
   //   Amount to guarantee: ${amount}
@@ -104,8 +132,6 @@ const func: DeployFunction = async function (bre: BuidlerRuntimeEnvironment) {
   // await UniPairWethDai.approve(poolAddress, parseEther('3000000000'));
   // const txAddOrder = await SLPool.stopLoss(parseEther('1000'), DAI_ADDRESS, parseEther('300'));
   // await txAddOrder.wait();
-  
-  
 
   return false; // when live network, record the script as executed to prevent rexecution
 };
