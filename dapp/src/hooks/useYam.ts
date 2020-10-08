ffefc326fe7e224e45fc6e8f142dcb8b74749377
dhadrien <hadrien@variabl.io>
2020-10-08 19:20:48 +0200

    First version of the dapp

--- dapp/src/hooks/useYam.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react'
-import { Context } from '../contexts/SLProvider'
-
-const useYam = () => {
-  const { sl } = useContext(Context)
-  return sl
-}
-
-export default useYam
